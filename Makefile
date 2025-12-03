122890e6028ea7c00373cfdcfc949ca51831b966
anon <anon@anon.anon>
2025-12-03 13:29:15 +0100

    reorganize the project

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ CFLAGS := -Ilibrary -D_GNU_SOURCE -std=c23 -Wall -ggdb
 LIB    := $$(pkg-config --cflags --libs tcl tk x11) -ltree-sitter -ltree-sitter-markdown -ltree-sitter-markdown-inline
 
 main:
-	./tbsp -o main.tbsp.c main.tbsp
+	./tbsp -o markdown.tbsp.c markdown.tbsp
 	./tbsp --prefix il -o inline.tbsp.c inline.tbsp
-	gcc ${CFLAGS} -o main.out main.tbsp.c inline.tbsp.c library/sds.c ${LIB}
+	gcc ${CFLAGS} -o main.out main.c gui.c markdown.tbsp.c inline.tbsp.c library/sds.c ${LIB}
--- main.c
+++ main.c
@@ -1,23 +1,55 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <tcl.h>
-#include <tk.h>
 #include <slurp.h>
+#include <sds.h>
+#include "gui.h"
+
+extern int tbtraverse(const char * const tbcode);
+
+sds script_buffer;
 
 static
-int Tcl_cGetCounter(TCL_ARGS) {
-    char r[12];
-    sprintf(r, "%ld", 10l);
-    Tcl_SetResult(interp, r, TCL_VOLATILE);
-    return TCL_OK;
+void usage(void) {
+    puts(
+        "markdown-gui <input-file>\n"
+        "  -h        : print help and exit\n"
+    );
 }
 
-
 signed main(int argc, char * argv[]) {
-    if (argc < 2) { return 1; }
+    // Init
+    const char * in_file = NULL;
 
+    if (argc < 2) {
+      usage_error:
+        usage();
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-h")
+        ||  !strcmp(argv[i], "--help")) {
+            usage();
+            return 0;
+        } else {
+            in_file = argv[i];
+        }
+    }
+
+    if (!in_file) {
+        goto usage_error;
+    }
+
+    char * in_str = slurp(in_file);
+
+    script_buffer = sdsnew("source \"lui.tcl\"\n");
+
+    // IoC
+    tbtraverse(in_str);
+
+    // Core
+    tcl_loop();
+    while (true) { usleep(50 * 1000); }
 
     return 0;
 }
